0109fe78ebff13896a7ff1aa7d4b3086de183ec7
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-05-18 17:13:15 -0300

    Add confirmed accommodation layout

--- MauiAppHotel/Views/BookingAccommodation.xaml.cs
+++ MauiAppHotel/Views/BookingAccommodation.xaml.cs
@@ -6,4 +6,16 @@ public partial class BookingAccommodation : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Navigation.PushAsync(new ConfirmedAccommodation());
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Oops", ex.Message, "OK");
+		}
+	}
 }
